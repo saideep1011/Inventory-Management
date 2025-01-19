e4a2dc3e7addfbe66f2c66ca86d2c2c2fd02607f
saideep1011 <saideep2311@gmail.com>
2025-01-19 19:07:02 +0530

    added user types

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,26 @@
 // src/Navbar.tsx
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar: React.FC = () => {
+  const [userType, setUserType] = useState("admin");
+  const handleUser = () => {
+    if (userType === "admin") {
+      setUserType("user");
+    } else {
+      setUserType("admin");
+    }
+  };
   return (
     <nav
       className="bg-black text-white p-4 fixed top-0 left-0 right-0 z-10"
       style={{ backgroundColor: "#161718" }}
     >
-      <div className="flex items-center justify-between ">
-        <h1 className="text-xl font-bold  w-full flex items-start justify-end">
-          Inventory Management
+      <div
+        className="flex items-center justify-between  cursor-pointer"
+        onClick={handleUser}
+      >
+        <h1 className="text-xl font-bold  w-full capitalize flex items-start justify-end">
+          {userType}
         </h1>
       </div>
     </nav>
